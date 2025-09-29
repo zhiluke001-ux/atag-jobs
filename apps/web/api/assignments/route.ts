b3c2afec5749e6cebf0b23829816e5f67eeb8f75
zhiluke001-ux <zhiluke001@gmail.com>
2025-09-29 17:41:22 +0800

    Update route.ts

--- apps/web/api/assignments/route.ts
+++ apps/web/api/assignments/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { prisma } from "../../../lib/prisma";
+import { prisma } from '@/lib/prisma';
 
 export const runtime = "nodejs";
 
