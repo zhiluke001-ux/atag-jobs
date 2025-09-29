e208d3ccc13003956debdee32c1daf6909ea0318
zhiluke001-ux <zhiluke001@gmail.com>
2025-09-29 17:48:34 +0800

    Update route.ts

--- apps/web/app/api/jobs/[id]/session/start/route.ts
+++ apps/web/app/api/jobs/[id]/session/start/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { redis } from "../../../../../lib/redis";
+import { redis } from '@/lib/redis';
 import { randomUUID } from "crypto";
 
 const SESSION_TTL_SECONDS = Number(process.env.SESSION_TTL_SECONDS || "21600");
