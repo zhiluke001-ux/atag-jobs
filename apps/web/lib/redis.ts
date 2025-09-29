17d53f4213523ec3da47cfc664019286c918b31e
zhiluke001-ux <zhiluke001@gmail.com>
2025-09-29 11:27:08 +0800

    Create redis.ts

---
+++ apps/web/lib/redis.ts
@@ -0,0 +1,8 @@
+import { Redis } from "@upstash/redis";
+
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!
+});
+
+export const ttl = (seconds: number) => seconds; // Upstash uses EX seconds directly in set
