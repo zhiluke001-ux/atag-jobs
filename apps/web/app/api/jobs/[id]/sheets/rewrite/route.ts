cc689bbf181a6f7b1ffedbd783202e5479134208
zhiluke001-ux <zhiluke001@gmail.com>
2025-09-29 17:40:59 +0800

    Update route.ts

--- apps/web/app/api/jobs/[id]/sheets/rewrite/route.ts
+++ apps/web/app/api/jobs/[id]/sheets/rewrite/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { prisma } from "../../../../../lib/prisma";
-import { computePay } from "../../../../../lib/pay";
+import { prisma } from '@/lib/prisma';
+import { computePay } from '@/lib/pay';
 import { google } from "googleapis";
 
 export const runtime = "nodejs";
